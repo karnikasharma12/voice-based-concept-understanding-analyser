603ab4d9fde520260168736d62a1993616368fa4
Parul Mathuria <parulmathuria3674@gmail.com>
2026-07-05 22:29:06 +0530

    Update semantic_analysis.py

--- semantic_analysis.py
+++ semantic_analysis.py
@@ -5,6 +5,6 @@ def  semantic_analysis(reference,student):
     emb1 = model.encode(reference, convert_to_tensor=True)
     emb2 = model.encode(student, convert_to_tensor=True)
 
-    score = round(util.cos_sim(emb1, emb2).item() * 100, 2)
+    score = round(util.cos_sim(emb1, emb2).item() * 100,2)
 
     return score
